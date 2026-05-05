149ba1a39888095297a9cf82dcaa36ada47f76e9
GideonMallinson <gideon.mallinson@eagles.oc.edu>
2026-05-05 16:01:44 -0500

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Plugin.cs
+ * 
+ * Initializes the mod and applies all patches on load
+ */
+
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace SkillFern
+{
+    [BepInPlugin("com.dajadeninja.repo.skillfern", "Skill Fern", "0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("com.dajadeninja.repo.skillfern"); // Harmony instance to patch with
+
+        /*
+         * Load all patches when the plugin wakes up
+         */
+        private void Awake()
+        {
+            // announce success and next step
+            Logger.LogInfo("Skill Fern loaded successfully! Loading patches. . .");
+
+            // apply all patches
+            harmony.PatchAll();
+        }
+    }
+}
