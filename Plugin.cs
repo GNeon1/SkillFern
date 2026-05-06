797356e11b0fe79e559c5e8b5747165cf5658e8c
GideonMallinson <gideon.mallinson@eagles.oc.edu>
2026-05-06 13:22:22 -0500

    Fixed hotloading configuration

--- Plugin.cs
+++ Plugin.cs
@@ -1,6 +1,6 @@
 ﻿/*
  * Plugin.cs
- * 
+ *  
  * Initializes the mod and applies all patches on load
  */
 
@@ -13,18 +13,32 @@ namespace SkillFern
     [BepInPlugin("com.dajadeninja.repo.skillfern", "Skill Fern", "0.0.0")]
     public class Plugin : BaseUnityPlugin
     {
-        private readonly Harmony harmony = new Harmony("com.dajadeninja.repo.skillfern"); // Harmony instance to patch with
+
+        private Harmony harmony; // Harmony instance to patch with
 
         /*
          * Load all patches when the plugin wakes up
          */
         private void Awake()
         {
+            // instantiate Harmony
+            harmony = new Harmony("com.dajadeninja.repo.skillfern");
+
             // announce success and next step
             Logger.LogInfo("Skill Fern loaded successfully! Loading patches. . .");
 
             // apply all patches
             harmony.PatchAll();
+        }
+
+        /*
+         * Unpatch when a hot-reload goes through
+         */
+        private void OnDestroy()
+        {
+            Logger.LogInfo("Skill Fern unloading. Cleaning up patches...");
+
+            harmony?.UnpatchSelf();
         }
     }
 }
