5d64cf79fbb625de52ad4eca74f57b5c2fbaf269
GideonMallinson <gideon.mallinson@eagles.oc.edu>
2026-05-06 14:29:58 -0500

    - Removed item upgrades from the shop. - Added DebugHelper with test functions. - Fixed Target Framework.

---
+++ Utilities/DebugHelper.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * DebugHelper.cs
+ *  
+ * Contains a variety of helpful debug functions to call from the C# console in-game
+ */
+
+namespace SkillFern.Utilities
+{
+    public class DebugHelper
+    {
+
+        /*
+         * Moves to a shop level with re-rolled items
+         */
+        public static void TestShop() {
+            RunManager.instance.ChangeLevel(true, false, RunManager.ChangeLevelType.Shop);
+        }
+    }
+}
